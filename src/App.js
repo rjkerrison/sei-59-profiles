2b37548a739a5d8415a9304bb3084c93d8654c7e
Hannah Hill <hannahhill@Hannahs-MacBook-Pro-3.local>
2021-10-27 09:36:55 +0100

    hannah

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Robin from './components/Robin'
+import Hannah from './components/Hannah'
 import './App.css'
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <Router>
       <Switch>
         <Route path='/robin' component={Robin} />
+        <Route path='/hannah' component={Hannah} />
       </Switch>
     </Router>
   )
---
+++ src/components/Hannah.js
@@ -0,0 +1,9 @@
+const Hannah = () => {
+  return (
+    <>
+      <h1>Hannah's Page</h1>
+    </>
+  )
+}
+
+export default Hannah

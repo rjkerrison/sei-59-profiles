48d216f7bb1406f8f6c863b7cbbec1c55ea81283
Chameh96 <edward.chamberlain2009@gmail.com>
2021-10-27 09:37:54 +0100

    ed added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Robin from './components/Robin'
 import './App.css'
+import Ed from './components/Ed'
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route path='/robin' component={Robin} />
+        <Route path='/ed' component={Ed} />
       </Switch>
     </Router>
   )
---
+++ src/components/Ed.js
@@ -0,0 +1,9 @@
+const Ed = () => {
+    return (
+      <>
+        <h1>Ed Chamberlain</h1>
+        <p>Student </p>
+      </>
+    )
+  }
+  export default Ed

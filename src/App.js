f66ba44e40baef05d632d5a40b503db19fb1f642
rosedunlop <rosedunlop@outlook.com>
2021-10-27 09:34:56 +0100

    Rose

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Robin from './components/Robin'
 import './App.css'
+import Rose from './components/Rose'
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route path='/robin' component={Robin} />
+        <Route path='/rose' component={Rose} />
       </Switch>
     </Router>
   )
---
+++ src/components/Rose.js
@@ -0,0 +1,11 @@
+
+const Rose = () => {
+    return (
+        <div>
+            <h1>Rose Dunlop</h1>
+            <p>Student</p>     
+        </div>
+    )
+}
+
+export default Rose

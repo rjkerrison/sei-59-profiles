ca4536de1171055c02db43bd54e1e8ce5d69ed4b
Reisli Hysa <reislihysa@Isabellas-iMac.local>
2021-10-27 09:41:37 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import Robin from './components/Robin'
-import './App.css'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Robin from "./components/Robin";
+import Reisli from "./components/Reisli";
+import "./App.css";
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route path='/robin' component={Robin} />
+        <Route path="/robin" component={Robin} />
+        <Route path="/reisli" component={Reisli} />
       </Switch>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Reisli.js
@@ -0,0 +1,9 @@
+const Reisli = () => {
+  return (
+    <>
+      <h1>Reisli Hysa</h1>
+      <p>Constant bed hair</p>
+    </>
+  );
+};
+export default Reisli;

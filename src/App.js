d8c3c43f9dfca72fc812c9cabd28de67954acbfb
Robin James Kerrison <7150842+rjkerrison@users.noreply.github.com>
2021-10-27 11:47:08 +0200

    styling and nav

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Robin from './components/Robin'
 import Arthur from './components/Arthur'
 import James from './components/James'
-import './App.css'
 import Monia from './components/Monia'
 import Home from './components/Home'
 
@@ -10,11 +9,11 @@ function App() {
   return (
     <Router>
       <Switch>
-        <Route path='/' component={Home} />
         <Route path='/robin' component={Robin} />
         <Route path='/arthur' component={Arthur} />
         <Route path='/monia' component={Monia} />
         <Route path='/james' component={James} />
+        <Route path='/' component={Home} />
       </Switch>
     </Router>
   )

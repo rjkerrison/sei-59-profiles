fe2ba7cc5beb45987c6f5453b677950376fad460
Robin James Kerrison <7150842+rjkerrison@users.noreply.github.com>
2021-10-27 12:36:26 +0200

    nav and styling

--- src/App.js
+++ src/App.js
@@ -4,15 +4,23 @@ import Arthur from './components/Arthur'
 import James from './components/James'
 import Monia from './components/Monia'
 import Home from './components/Home'
+import Nav from './components/Nav'
 
 function App() {
+  const links = [
+    { component: Robin, path: '/robin', name: 'Robin' },
+    { component: Arthur, path: '/arthur', name: 'Arthur' },
+    { component: Monia, path: '/monia', name: 'Monia' },
+    { component: James, path: '/james', name: 'James' },
+  ]
+
   return (
     <Router>
+      <Nav links={links} />
       <Switch>
-        <Route path='/robin' component={Robin} />
-        <Route path='/arthur' component={Arthur} />
-        <Route path='/monia' component={Monia} />
-        <Route path='/james' component={James} />
+        {links.map((props) => (
+          <Route {...props} />
+        ))}
         <Route path='/' component={Home} />
       </Switch>
     </Router>

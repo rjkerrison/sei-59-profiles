76368d395ae6abd21d0b1d21d6efc3a9ab345991
Milo Bedini <milobedini64@gmail.com>
2021-10-27 09:34:57 +0100

    Milo added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import Robin from './components/Robin'
-import './App.css'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import Robin from "./components/Robin"
+import "./App.css"
+import Milo from "./components/Milo"
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route path='/robin' component={Robin} />
+        <Route path="/robin" component={Robin} />
+        <Route path="/milo" component={Milo} />
       </Switch>
     </Router>
   )
---
+++ src/components/Milo.js
@@ -0,0 +1,10 @@
+const Milo = () => {
+  return (
+    <>
+      <h1>Milo Bedini</h1>
+      <p>Award Winning Student</p>
+    </>
+  )
+}
+
+export default Milo
